34b1c9bde06b46dd808af9eb3b600e73499a2429
Coder-Yu <coderyy@outlook.com>
2017-01-25 01:54:41 +0800

    add unigram and tf-idf

--- tf_idf.py
+++ tf_idf.py
@@ -18,50 +18,46 @@ class Texts(object):
     def __init__(self, sources):
         self.sources = sources
 
-        flipped = {}
-
-        # make sure that keys are unique
-        for key, value in sources.items():
-            if value not in flipped:
-                flipped[value] = [key]
-            else:
-                raise Exception('Non-unique prefix encountered')
 
     def to_vector(self):
         self.sentences = []
-        self.tags = []
-        for source, prefix in self.sources.items():
+        for source in self.sources:
             with utils.smart_open(source) as fin:
                 for item_no, line in enumerate(fin):
                     words = utils.to_unicode(line).split()
                     words = [word for word in words if word not in stoplist]
                     self.sentences.append(words)
-                    self.tags.append(prefix)
-        return self.sentences,self.tags
+        return self.sentences
 
 
 #create source
-sources = {}
+sources = []
+labels = []
+
 trainingPath = './dataset/trainingset/'
 for home, dirs, files in walk(trainingPath+'positive'):
-    for i,filename in enumerate(files):
-        sources[home+'/'+filename] = 'TRAIN_POS_'+str(i)
+    for filename in files:
+        sources.append(home+'/'+filename)
+        labels.append(1)
 
 for home, dirs, files in walk(trainingPath+'negative'):
-    for i,filename in enumerate(files):
-        sources[home+'/'+filename] = 'TRAIN_NEG_'+str(i)
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(0)
 
 testPath = './dataset/testset/'
 for home, dirs, files in walk(testPath + 'positive'):
-    for i, filename in enumerate(files):
-        sources[home+'/'+filename] = 'TEST_POS_' + str(i)
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(1)
 
 for home, dirs, files in walk(testPath + 'negative'):
-    for i, filename in enumerate(files):
-        sources[home+'/'+filename] = 'TEST_NEG_' + str(i)
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(0)
 
 
-corpus,tagNames = Texts(sources).to_vector()
+corpus = Texts(sources).to_vector()
 dictionary = corpora.Dictionary(corpus)
 corpus = [dictionary.doc2bow(text) for text in corpus]
 model = models.TfidfModel(corpus)
@@ -70,40 +66,15 @@ text_matrix = gensim.matutils.corpus2dense(corpus,num_terms = len(dictionary.tok
 # pca = PCA(n_components=50)
 # text_matrix = pca.fit_transform(text_matrix)
 
-train_arrays = numpy.zeros((640, len(dictionary.token2id)))
-train_labels = numpy.zeros(640)
-
-test_arrays = numpy.zeros((160, len(dictionary.token2id)))
-test_labels = numpy.zeros(160)
-
-train_count=0
-test_count=0
-
-for i,tag in enumerate(tagNames):
-    if 'TRAIN' in tag:
-        train_arrays[train_count] = text_matrix[i]
-        if 'POS' in tag:
-            train_labels[train_count] = 1
-        else:
-            train_labels[train_count] = 0
-        train_count += 1
-    elif 'TEST' in tag:
-        test_arrays[test_count] = text_matrix[i]
-        if 'POS' in tag:
-            test_labels[test_count] = 1
-        else:
-            test_labels[test_count] = 0
-        test_count += 1
-
 
 classifier = LogisticRegression()
-classifier.fit(train_arrays, train_labels)
-pred_labels = classifier.predict(test_arrays)
+classifier.fit(text_matrix[:640], labels[:640])
+pred_labels = classifier.predict(text_matrix[640:])
 print 'Logistic:'
-print classification_report(test_labels,pred_labels)
+print classification_report(labels[640:],pred_labels)
 
 classifier = SVC()
-classifier.fit(train_arrays, train_labels)
-pred_labels = classifier.predict(test_arrays)
+classifier.fit(text_matrix[:640], labels[:640])
+pred_labels = classifier.predict(text_matrix[640:])
 print 'SVM:'
-print classification_report(test_labels,pred_labels)
+print classification_report(labels[640:],pred_labels)
